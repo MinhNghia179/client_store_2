48128097d69960e0e93d06fbbeba9f593d332591
Levi Do <vinh.dothanh@powergatesoftware.com>
2022-04-29 22:15:22 +0700

    fix bug

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,15 +1,15 @@
 export const formatPrice = (number) => {
-  return new Intl.NumberFormat('en-US', {
+  return Intl.NumberFormat('vi-VN', {
     style: 'currency',
-    currency: 'USD',
-  }).format(number / 100)
-}
+    currency: 'VND',
+  }).format(number);
+};
 
 export const getUniqueValues = (data, type) => {
-  let unique = data.map((item) => item[type])
+  let unique = data.map((item) => item[type]);
   if (type === 'colors') {
-    unique = unique.flat()
+    unique = unique.flat();
   }
 
-  return ['all', ...new Set(unique)]
-}
+  return ['all', ...new Set(unique)];
+};
